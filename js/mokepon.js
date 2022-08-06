72f09977f4000151cf963f37735f3ac4d440b789
a9leom <a9leom@gmail.com>
2022-08-05 21:58:22 -0500

    Proceso de escuhcar eventos con JavaScript

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,9 +1,14 @@
-// Función a ejecutar tras suceder el evento
+// Función a ejecutar tras haberse cargado el HTML
+function iniciarJuego() {
+    // Seleccionando desde JS un elemento de HTML
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    // Escuchando evento click del elemento botón
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+// Función a ejecutar tras dar click al botón
 function seleccionarMascotaJugador() {
     alert("Seleccionaste tu mascota")
 }
 
-// Seleccionando desde JS un elemento de HTML
-let botonMascotaJugador = document.getElementById("boton-mascota")
-// Escuchando evento click del botón
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+/* Escuchar evento del objeto window. Con load pedimos al navegador que avise cunado el HTML ya haya cargado */
+window.addEventListener('load', iniciarJuego)
