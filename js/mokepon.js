1f390ee1116131d8cb86d90f6ef908425b75dfd4
a9leom <a9leom@gmail.com>
2022-08-06 20:12:09 -0500

    Código a ejecutar tras seleccionar una mascota

--- js/mokepon.js
+++ js/mokepon.js
@@ -5,9 +5,21 @@ function iniciarJuego() {
     // Escuchando evento click del elemento botón
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
-// Función a ejecutar tras dar click al botón
+// Función a ejecutar tras dar click al botón seleccionar (mascota)
 function seleccionarMascotaJugador() {
-    alert("Seleccionaste tu mascota")
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if (inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigüeya')
+    } else {
+        alert('Debes seleccionar una mascota')
+    }
 }
 
 /* Escuchar evento del objeto window. Con load pedimos al navegador que avise cunado el HTML ya haya cargado */
